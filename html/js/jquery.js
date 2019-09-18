cf7a22b5267d0ff808d1f14023f4390282baa7bc
alohaisai <mm70aloha@gmail.com>
2019-09-18 16:59:13 +0900

    ほぼ完成

--- html/js/jquery.js
+++ html/js/jquery.js
@@ -1,6 +1,41 @@
+// メインビジュアル画像切り替え
 $(function () {
   $(".fade-img img:not(:first-child)").hide();
   setInterval(function () {
     $(".fade-img img:first-child").fadeOut("slow").next("img").fadeIn("slow").end().appendTo(".fade-img");
   }, 5000);
+
+  // スムーズスクロール トップに戻る
+  $('.btn-wrapper').on('click', function () {
+    $('body,html').animate({ scrollTop: 0 }, 500);
+    return false;
+  });
+
+  // スクロールボタンで.profileまでスクロール
+  $('.main-visual__scroll').on('click', function () {
+    $('.container').slideDown(800);
+    $("html,body").animate({ scrollTop: $('.profile').offset().top }, 2000);
+    return false;
+  });
+
+  // PROFILEボタンで.profileまでスクロール
+  $('.profile-btn').on('click', function () {
+    $('.container').slideDown(800);
+    $("html,body").animate({ scrollTop: $('.profile').offset().top }, 2000);
+    return false;
+  });
+
+  // WORKSボタンで.workまでスクロール
+  $('.works-btn').on('click', function () {
+    $('.container').slideDown(800);
+    $("html,body").animate({ scrollTop: $('.work').offset().top }, 2000);
+    return false;
+  });
+
+  // CONTACTボタンで.contactまでスクロール
+  $('.contact-btn').on('click', function () {
+    $('.container').slideDown(800);
+    $("html,body").animate({ scrollTop: $('.contact').offset().top }, 2000);
+    return false;
+  });
 });
